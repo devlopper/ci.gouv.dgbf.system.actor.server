f7214ea5d324af4f2232a0f07924fbd214538d29
Christian Yao Komenan <kycdev@gmail.com>
2021-01-03 19:23:40 +0000

    commit

--- actor-server-persistence-entities/src/main/java/ci/gouv/dgbf/system/actor/server/persistence/entities/PhysicalNamingStrategyStandardImpl.java
+++
@@ -1,5 +0,0 @@
-package ci.gouv.dgbf.system.actor.server.persistence.entities;
-
-public class PhysicalNamingStrategyStandardImpl extends org.cyk.utility.__kernel__.object.__static__.persistence.hibernate.PhysicalNamingStrategyStandardImpl {
-
-}
