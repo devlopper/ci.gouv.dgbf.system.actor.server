7f2f42ea3dc2cdc81727bb7015190fad84795617
Christian Yao Komenan <kycdev@gmail.com>
2021-05-24 13:33:14 +0000

    commit

--- actor-server-business-impl/src/main/java/ci/gouv/dgbf/system/actor/server/business/impl/ActorProfileBusinessImpl.java
+++ actor-server-business-impl/src/main/java/ci/gouv/dgbf/system/actor/server/business/impl/ActorProfileBusinessImpl.java
@@ -4,14 +4,17 @@ import java.io.Serializable;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import org.cyk.utility.__kernel__.log.LogHelper;
+import org.cyk.utility.__kernel__.properties.Properties;
+import org.cyk.utility.__kernel__.string.StringHelper;
+import org.cyk.utility.security.keycloak.server.UserManager;
+import org.cyk.utility.server.business.AbstractBusinessEntityImpl;
+import org.cyk.utility.server.business.BusinessFunctionCreator;
+import org.cyk.utility.server.business.BusinessFunctionRemover;
+
 import ci.gouv.dgbf.system.actor.server.business.api.ActorProfileBusiness;
 import ci.gouv.dgbf.system.actor.server.persistence.api.ActorProfilePersistence;
 import ci.gouv.dgbf.system.actor.server.persistence.entities.ActorProfile;
-
-import org.cyk.utility.__kernel__.properties.Properties;
-import org.cyk.utility.__kernel__.string.StringHelper;
-import org.cyk.utility.server.business.AbstractBusinessEntityImpl;
-import org.cyk.utility.server.business.BusinessFunctionCreator;
 
 @ApplicationScoped
 public class ActorProfileBusinessImpl extends AbstractBusinessEntityImpl<ActorProfile, ActorProfilePersistence> implements ActorProfileBusiness,Serializable {
@@ -24,4 +27,23 @@ public class ActorProfileBusinessImpl extends AbstractBusinessEntityImpl<ActorPr
 			actorProfile.setIdentifier(actorProfile.getActor().getCode()+"_"+actorProfile.getProfile().getCode());
 	}
 	
+	@Override
+	protected void __listenExecuteCreateAfter__(ActorProfile actorProfile, Properties properties,BusinessFunctionCreator function) {
+		super.__listenExecuteCreateAfter__(actorProfile, properties, function);
+		try {
+			UserManager.getInstance().addRolesByNames(actorProfile.getActor().getCode(), actorProfile.getProfile().getCode());			
+		} catch (Exception exception) {
+			LogHelper.logWarning(String.format("role %s not added in keycloak on user %s",actorProfile.getProfile().getCode(),actorProfile.getActor().getCode()), getClass());
+		}
+	}
+	
+	@Override
+	protected void __listenExecuteDeleteAfter__(ActorProfile actorProfile, Properties properties,BusinessFunctionRemover function) {
+		super.__listenExecuteDeleteAfter__(actorProfile, properties, function);
+		try {
+			UserManager.getInstance().deleteRolesByNames(actorProfile.getActor().getCode(), actorProfile.getProfile().getCode());			
+		} catch (Exception exception) {
+			LogHelper.logWarning(String.format("role %s not deleted in keycloak on user %s",actorProfile.getProfile().getCode(),actorProfile.getActor().getCode()), getClass());
+		}
+	}
 }
