6b3bc510e7e5b6e7fea550162770cd99ffcfbc66
devlopper <kycdev@gmail.com>
2022-12-13 17:17:41 +0000

    commit

---
+++ actor-server-deployment/script/git/commit_push_to_dev.bat
@@ -0,0 +1,17 @@
+@echo off
+echo #### timestamp computation
+FOR /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined MyDate set MyDate=%%x
+FOR /f %%x in ('wmic path win32_localtime get /format:list ^| findstr "="') do set %%x
+SET fmonth=00%Month%
+SET fday=00%Day%
+SET mydate=%Year%%fmonth:~-2%%fday:~-2%
+For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a%%b)
+set mytime=%mytime: =0%
+SET timestamp=v0.0.0-%mydate%%mytime%
+
+SET commit_message="actor server using bootable publishing %timestamp%"
+
+echo #### cloture server impl publishing
+git add .
+git commit --all -m %commit_message%
+git push dgbf dev:dev
